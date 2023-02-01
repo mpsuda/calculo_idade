3c41a4856793683f4f9adab48b7c643033834ed2
Marcos Suda <marcos.suda@gmail.com>
2023-02-01 20:03:12 -0300

    Calculo Idade

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Executei o JavaScript");
+
+function calcularIdade(){
+var ano_nascimento = prompt("Em que ano você nasceu?");
+
+var idade = 2023 - ano_nascimento;
+
+alert(idade);
+}
+
+
+
